a1074e7d1c0a5bf2943b3846bc268c47377e5926
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2020-02-10 19:51:09 +0400

    калькулятор треугольников
    с комментариями

---
+++ калькулятор/калькулятор/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace калькулятор
+{
+    class Class1
+    {
+        public int Perimeter(int a, int b, int c) => a + b + c;//Считает периметр
+        /// <summary>
+        /// нахоидит площадь
+        /// </summary>
+        /// <param name="a">Сторона треугольника</param>
+        /// <param name="b">Сторона треугольника</param>
+        /// <param name="c">Сторона треугольника</param>
+        /// <returns></returns>
+        
+        public double Area(int a, int b, int c)
+        {
+            var P = (a + b + c) / 2;
+            double S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+            return S;
+        }
+
+        /// <summary>
+        /// находит радиус вписанной окружности
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public double Area_of_inner_circle(int a, int b, int c)
+        {
+            var P = (a + b + c) / 2;
+            double R = Area(a, b, c) / P;
+            double area_of_inner_circle = 3.14 * Math.Pow(R, 2);
+            return area_of_inner_circle;
+        }
+
+        /// <summary>
+        /// проверяет корректность введённых данных
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int Check(int a, int b, int c)
+        {
+            if (a > 0 & b > 0 & c > 0)
+            {
+                if ((b + c) > a & (a + c) > b & (a + b) > c)
+                {
+                    return Perimeter(a, b, c);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// не реализованный метод по нахождению углов
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public double the_Angles_Of_A_Triangle(int a, int b, int c)
+        {
+            //Угол A
+            double pi = 3;
+            double A1 = ((b * b + c * c - a * a) / (2d * b * c));
+            double A2 = (double)Math.Acos(A1);
+            double A3 = (double)b * (double)Math.Sin(A2) / c;
+            double B1 = (double)Math.Asin(A3) * 180d / pi;
+            double B2 = A2 * 180d / pi;
+            double C1 = 180d - A2 - B1;
+            return B2;
+            //double alfa, beta;
+            //double alfa1, beta1, gamma;
+            //alfa = ((Math.Pow(b, 2) + Math.Pow(c, 2) - Math.Pow(a, 2)) / (2 * b * c));
+            //alfa1 = Math.Acos(alfa);
+            //beta = Math.Abs((Math.Pow(a, 2) + Math.Pow(c, 2) - Math.Pow(b, 2)) / (2 * a * c));
+            //beta1 = Math.Acos(beta);
+            //gamma = 180 - (alfa1 + beta1);
+            //Console.WriteLine($"Угол Аlfa = {alfa1} \n Угол Вeta = {beta1} \n Угол Gamma = {gamma}");         
+            //return alfa1;            
+        } //Метод для нахождение углов треугольника по сторона. НЕ РЕАЛИЗОВАН!
+    }
+}
---
+++ калькулятор/калькулятор/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace калькулятор
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 pipio = new Class1();//ОБьявление класса
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_a_TextChanged(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// выводит ответы или ошибку
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                                    
+            if (TextBox_a.Text != string.Empty && TextBox_b.Text != string.Empty && TextBox_c.Text != string.Empty)
+            {
+                if (!Char.IsDigit(TextBox_a.Text, 0) || !Char.IsDigit(TextBox_b.Text, 0) || !Char.IsDigit(TextBox_c.Text, 0)) //Провека что чувак ввёл числа а не буквы
+                {
+                    MessageBox.Show("были введены некоретные данные");
+                    TextBox_a.Clear();
+                    TextBox_b.Clear(); //Очищает textBox
+                    TextBox_c.Clear();
+                    TextBlock_a.Text = null;
+                    TextBlock_b.Text = null;
+                    TextBlock_c.Text = null;
+                }
+                else
+                {
+                    var a = Convert.ToInt32(TextBox_a.Text); //Передаёт значение с  textBox в перменную
+                    var b = Convert.ToInt32(TextBox_b.Text); //Передаёт значение с  textBox в перменную
+                    var c = Convert.ToInt32(TextBox_c.Text); //Передаёт значение с  textBox в перменную
+                    if (pipio.Check(a, b, c) != 0)
+                    {
+                        this.Otvet.Text = ($"P = {pipio.Check(a, b, c)}" +
+                        $"\nS = { Math.Round(pipio.Area(a, b, c), 3) }" +
+                        $"\nR вписанной окружности = { Math.Round(pipio.Area_of_inner_circle(a, b, c), 3)}");                    
+                        TextBlock_a.Text = TextBox_a.Text;
+                        TextBlock_b.Text = TextBox_b.Text;
+                        TextBlock_c.Text = TextBox_c.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("были введены некоретные данные");
+                        TextBlock_a.Text = null;
+                        TextBlock_b.Text = null;
+                        TextBlock_c.Text = null;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ячейки не могут быть пустыми");
+                TextBox_a.Clear();
+                TextBox_b.Clear();
+                TextBox_c.Clear();
+                TextBlock_a.Text = null;
+                TextBlock_b.Text = null;
+                TextBlock_c.Text = null;
+            }
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.D0)
+            {
+                    TextBlock_a.Text = TextBox_a.Text; //если нажата цифра то передаёт данные с textBox в textBlock
+            }
+
+        } // метод для отслеживания нажатий. НЕ РЕАЛИЗОВАННА ДО КОНЦА
+        private void TextBox_a_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        } 
+        private void TextBox_KeyDown1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.D0)
+            {
+                TextBlock_b.Text = TextBox_b.Text;
+            }
+        } // метод для отслеживания нажатий. НЕ РЕАЛИЗОВАННА ДО КОНЦА
+        private void TextBox_b_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        } 
+        private void TextBox_KeyDown2(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.D0)
+            {
+                TextBlock_c.Text = TextBox_c.Text;
+            }
+        } // метод для отслеживания нажатий. НЕ РЕАЛИЗОВАННА ДО КОНЦА
+        private void TextBox_c_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        } 
+    }
+}
